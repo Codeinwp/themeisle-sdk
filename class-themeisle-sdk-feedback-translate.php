11ae26b56064123e761e6ba253e12e97e39fd638
selu91 <cristea.marius.alexandru@gmail.com>
2017-09-26 16:46:11 +0300

    Improved translation check and list of slug by locales. Ref @contactashish13

--- class-themeisle-sdk-feedback-translate.php
+++ class-themeisle-sdk-feedback-translate.php
@@ -42,6 +42,207 @@ if ( ! class_exists( 'ThemeIsle_SDK_Feedback_Translate' ) ) :
 		}
 
 		/**
+		 * Return the locale path.
+		 *
+		 * @param string $locale Locale code.
+		 *
+		 * @return string Locale path.
+		 */
+		private function get_locale_paths( $locale ) {
+			if ( empty( $locale ) ) {
+				return '';
+			}
+			$locales = array(
+				'pt_PT_ao90'     => 'pt/ao90',
+				'de_CH_informal' => 'de-ch/informal',
+				'nl_NL_formal'   => 'nl/formal',
+				'de_DE_formal'   => 'de/formal',
+				'af'             => 'af',
+				'ak'             => 'ak',
+				'am'             => 'am',
+				'ar'             => 'ar',
+				'arq'            => 'arq',
+				'ary'            => 'ary',
+				'as'             => 'as',
+				'ast'            => 'ast',
+				'az'             => 'az',
+				'azb'            => 'azb',
+				'az_TR'          => 'az-tr',
+				'ba'             => 'ba',
+				'bal'            => 'bal',
+				'bcc'            => 'bcc',
+				'bel'            => 'bel',
+				'bg_BG'          => 'bg',
+				'bn_BD'          => 'bn',
+				'bo'             => 'bo',
+				'bre'            => 'br',
+				'bs_BA'          => 'bs',
+				'ca'             => 'ca',
+				'ceb'            => 'ceb',
+				'ckb'            => 'ckb',
+				'co'             => 'co',
+				'cs_CZ'          => 'cs',
+				'cy'             => 'cy',
+				'da_DK'          => 'da',
+				'de_DE'          => 'de',
+				'de_CH'          => 'de-ch',
+				'dv'             => 'dv',
+				'dzo'            => 'dzo',
+				'el'             => 'el',
+				'art_xemoji'     => 'art-xemoji',
+				'en_US'          => 'en',
+				'en_AU'          => 'en-au',
+				'en_CA'          => 'en-ca',
+				'en_GB'          => 'en-gb',
+				'en_NZ'          => 'en-nz',
+				'en_ZA'          => 'en-za',
+				'eo'             => 'eo',
+				'es_ES'          => 'es',
+				'es_AR'          => 'es-ar',
+				'es_CL'          => 'es-cl',
+				'es_CO'          => 'es-co',
+				'es_CR'          => 'es-cr',
+				'es_GT'          => 'es-gt',
+				'es_MX'          => 'es-mx',
+				'es_PE'          => 'es-pe',
+				'es_PR'          => 'es-pr',
+				'es_VE'          => 'es-ve',
+				'et'             => 'et',
+				'eu'             => 'eu',
+				'fa_IR'          => 'fa',
+				'fa_AF'          => 'fa-af',
+				'fuc'            => 'fuc',
+				'fi'             => 'fi',
+				'fo'             => 'fo',
+				'fr_FR'          => 'fr',
+				'fr_BE'          => 'fr-be',
+				'fr_CA'          => 'fr-ca',
+				'frp'            => 'frp',
+				'fur'            => 'fur',
+				'fy'             => 'fy',
+				'ga'             => 'ga',
+				'gd'             => 'gd',
+				'gl_ES'          => 'gl',
+				'gn'             => 'gn',
+				'gsw'            => 'gsw',
+				'gu'             => 'gu',
+				'hat'            => 'hat',
+				'hau'            => 'hau',
+				'haw_US'         => 'haw',
+				'haz'            => 'haz',
+				'he_IL'          => 'he',
+				'hi_IN'          => 'hi',
+				'hr'             => 'hr',
+				'hu_HU'          => 'hu',
+				'hy'             => 'hy',
+				'id_ID'          => 'id',
+				'ido'            => 'ido',
+				'is_IS'          => 'is',
+				'it_IT'          => 'it',
+				'ja'             => 'ja',
+				'jv_ID'          => 'jv',
+				'ka_GE'          => 'ka',
+				'kab'            => 'kab',
+				'kal'            => 'kal',
+				'kin'            => 'kin',
+				'kk'             => 'kk',
+				'km'             => 'km',
+				'kn'             => 'kn',
+				'ko_KR'          => 'ko',
+				'kir'            => 'kir',
+				'lb_LU'          => 'lb',
+				'li'             => 'li',
+				'lin'            => 'lin',
+				'lo'             => 'lo',
+				'lt_LT'          => 'lt',
+				'lv'             => 'lv',
+				'me_ME'          => 'me',
+				'mg_MG'          => 'mg',
+				'mk_MK'          => 'mk',
+				'ml_IN'          => 'ml',
+				'mlt'            => 'mlt',
+				'mn'             => 'mn',
+				'mr'             => 'mr',
+				'mri'            => 'mri',
+				'ms_MY'          => 'ms',
+				'my_MM'          => 'mya',
+				'ne_NP'          => 'ne',
+				'nb_NO'          => 'nb',
+				'nl_NL'          => 'nl',
+				'nl_BE'          => 'nl-be',
+				'nn_NO'          => 'nn',
+				'oci'            => 'oci',
+				'ory'            => 'ory',
+				'os'             => 'os',
+				'pa_IN'          => 'pa',
+				'pl_PL'          => 'pl',
+				'pt_BR'          => 'pt-br',
+				'pt_PT'          => 'pt',
+				'ps'             => 'ps',
+				'rhg'            => 'rhg',
+				'ro_RO'          => 'ro',
+				'roh'            => 'roh',
+				'ru_RU'          => 'ru',
+				'rue'            => 'rue',
+				'rup_MK'         => 'rup',
+				'sah'            => 'sah',
+				'sa_IN'          => 'sa-in',
+				'scn'            => 'scn',
+				'si_LK'          => 'si',
+				'sk_SK'          => 'sk',
+				'sl_SI'          => 'sl',
+				'sna'            => 'sna',
+				'snd'            => 'snd',
+				'so_SO'          => 'so',
+				'sq'             => 'sq',
+				'sq_XK'          => 'sq-xk',
+				'sr_RS'          => 'sr',
+				'srd'            => 'srd',
+				'su_ID'          => 'su',
+				'sv_SE'          => 'sv',
+				'sw'             => 'sw',
+				'syr'            => 'syr',
+				'szl'            => 'szl',
+				'ta_IN'          => 'ta',
+				'ta_LK'          => 'ta-lk',
+				'tah'            => 'tah',
+				'te'             => 'te',
+				'tg'             => 'tg',
+				'th'             => 'th',
+				'tir'            => 'tir',
+				'tl'             => 'tl',
+				'tr_TR'          => 'tr',
+				'tt_RU'          => 'tt',
+				'tuk'            => 'tuk',
+				'twd'            => 'twd',
+				'tzm'            => 'tzm',
+				'ug_CN'          => 'ug',
+				'uk'             => 'uk',
+				'ur'             => 'ur',
+				'uz_UZ'          => 'uz',
+				'vi'             => 'vi',
+				'wa'             => 'wa',
+				'xho'            => 'xho',
+				'xmf'            => 'xmf',
+				'yor'            => 'yor',
+				'zh_CN'          => 'zh-cn',
+				'zh_HK'          => 'zh-hk',
+				'zh_TW'          => 'zh-tw',
+			);
+
+			$slug = isset( $locales[ $locale ] ) ? $locales[ $locale ] : '';
+			if ( empty( $slug ) ) {
+				return '';
+			}
+			if ( strpos( $slug, '/' ) === false ) {
+				$slug .= '/default';
+			}
+			$url = 'https://translate.wordpress.org/projects/wp-' . $this->product->get_type() . 's/' . $this->product->get_slug() . '/dev/' . $slug . '?filters%5Bstatus%5D=untranslated&sort%5Bby%5D=random';
+
+			return $url;
+		}
+
+		/**
 		 * Registers the hooks
 		 */
 		public function setup_hooks_child() {
@@ -74,7 +275,7 @@ if ( ! class_exists( 'ThemeIsle_SDK_Feedback_Translate' ) ) :
 
 			$this->add_css( $this->product->get_key() );
 			$this->add_js( $this->product->get_key() );
-			$html   = $this->get_html( $this->product->get_key() );
+			$html = $this->get_html( $this->product->get_key() );
 
 			if ( $html ) {
 				echo '<div class="notice notice-success is-dismissible" id="' . $id . '" ><div class="themeisle-translate-box">' . $html . '</div></div>';
@@ -126,30 +327,42 @@ if ( ! class_exists( 'ThemeIsle_SDK_Feedback_Translate' ) ) :
 		/**
 		 * Generates the HTML
 		 *
-		 * @param string $key The product key.
+		 * @param   string $key The product key.
+		 *
+		 * @return  void|string Html code of the notification.
 		 */
 		function get_html( $key ) {
-			$lang    = defined( 'WP_LANG' ) ? WP_LANG : get_option( 'WPLANG', 'en_US' );
+			$lang = get_user_locale();
 			if ( 'en_US' === $lang ) {
 				return;
 			}
+			$array = explode( '_', $lang );
+			require_once( ABSPATH . 'wp-admin/includes/translation-install.php' );
 
-			$array  = explode( '_', $lang );
+			$languages = translations_api(
+				$this->product->get_type() . 's',
+				array(
+					'slug'    => $this->product->get_slug(),
+					'version' => $this->product->get_version(),
+				)
+			);
 
-			$link    = sprintf( 'https://translate.wordpress.org/projects/wp-' . $this->product->get_type() . 's/' . $this->product->get_slug() . '/dev/%s/default/', $array[0] );
+			if ( ! isset( $languages['translations'] ) ) {
+				return;
+			}
+			$languages = $languages['translations'];
+			$available = wp_list_pluck( $languages, 'language' );
+			if ( in_array( $lang, $available ) ) {
+				return;
+			}
+			$link    = $this->get_locale_paths( $lang );
 			$heading = apply_filters( $this->product->get_key() . '_feedback_translate_heading', $this->heading );
 			$heading = str_replace(
 				array( '{product}' ),
 				trim( str_replace( 'Lite', '', apply_filters( $this->product->get_key() . '_friendly_name', $this->product->get_name() ) ) ), $heading
 			);
 
-			$message    = apply_filters( $this->product->get_key() . '_feedback_translation_no', 'Transations not available' );
-			$file       = dirname( $this->product->get_basefile() ) . '/languages/' . $this->product->get_slug() . '-' . $lang . '.mo';
-
-			if ( file_exists( $file ) ) {
-				// translations available
-				$message    = apply_filters( $this->product->get_key() . '_feedback_translation_yes', 'Transations incomplete' );
-			}
+			$message = apply_filters( $this->product->get_key() . '_feedback_translation_no', 'Transations not available' );
 
 			$button_cancel = apply_filters( $this->product->get_key() . '_feedback_translate_button_cancel', $this->button_cancel );
 			$button_do     = apply_filters( $this->product->get_key() . '_feedback_translate_button_do', $this->button_do );
