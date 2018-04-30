ae7fce00ef4fdec1b3f5b918ee0b5e9fe18d588f
selu91 <cristea.marius.alexandru@gmail.com>
2018-04-30 14:29:58 +0300

    Bump version.

--- load.php
+++ load.php
@@ -11,7 +11,7 @@
  */
 
 // Current SDK version and path.
-$themeisle_sdk_version = '2.2.1';
+$themeisle_sdk_version = '2.2.3';
 $themeisle_sdk_path    = dirname( __FILE__ );
 
 global $themeisle_sdk_max_version;
