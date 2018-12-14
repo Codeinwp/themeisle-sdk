4047c5c6b64a14190a4fe2a8d9cea43c895ccc5c
selul <marius.cristea@vertistudio.com>
2018-12-14 20:26:44 +0200

    build: fix testsuite install

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+composer global require "phpunit/phpunit=4.8.*|5.7.*"
 bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
 phpunit || exit 1
 WP_MULTISITE=1 phpunit || exit 1
