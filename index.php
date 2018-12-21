74b79f888e14ace771202a616c1701da231b4b1a
selul <marius.cristea@vertistudio.com>
2018-12-21 20:15:05 +0200

    build: fix exported files and remove redundant code

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
 // phpcs:ignoreFile
 // Nothing here.
-
-/**
- * Plugin Name:       Sample plugin.
- * Description:       Sample description
- * Version:           1.1.1
- * Author:            ThemeIsle
- * Author URI:        https://themeisle.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       sample-plugin
- * WordPress Available:  no
- * Requires License:    no
- */
-include "load.php";
