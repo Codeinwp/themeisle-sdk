44551d4c9723d776751e0b24a821a127ba9bcade
Ashish Ravi <contactashish13@gmail.com>
2019-02-28 16:30:36 +0530

    slack comments
    1) translations to be provided by the plugin
    2) thickbox popup for details

--- src/Modules/Upsell.php
+++ src/Modules/Upsell.php
@@ -53,7 +53,7 @@ class Upsell extends Abstract_Module {
 	 * Setup endpoints.
 	 */
 	private function setup_hooks() {
-		add_action( $this->product->get_key() . '_upsell_products', array( $this, 'render_products_box' ), 10, 3 );
+		add_action( $this->product->get_key() . '_upsell_products', array( $this, 'render_products_box' ), 10, 4 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
 	}
 
@@ -62,9 +62,10 @@ class Upsell extends Abstract_Module {
 	 *
 	 * @param array $plugins_list - list of useful plugins (in slug => nicename format).
 	 * @param array $themes_list - list of useful themes (in slug => nicename format).
+	 * @param array $string - list of translated strings.
 	 * @param array $preferences - list of preferences.
 	 */
-	function render_products_box( $plugins_list, $themes_list, $preferences = array() ) {
+	function render_products_box( $plugins_list, $themes_list, $strings, $preferences = array() ) {
 		if ( empty( $plugins_list ) && empty( $themes_list ) ) {
 			return;
 		}
@@ -76,6 +77,8 @@ class Upsell extends Abstract_Module {
 		if ( ! empty( $themes_list ) && ! current_user_can( 'install_themes' ) ) {
 			return;
 		}
+
+		add_thickbox();
 
 		if ( ! empty( $plugins_list ) ) {
 			echo '<div class="upsell-product">';
@@ -90,6 +93,17 @@ class Upsell extends Abstract_Module {
 					$image = $current_plugin->icons['1x'];
 				}
 
+				$url = add_query_arg(
+					array(
+						'tab'       => 'plugin-information',
+						'plugin'    => $current_plugin->slug,
+						'TB_iframe' => true,
+						'width'     => 600,
+						'height'    => 500,
+					),
+					network_admin_url( 'plugin-install.php' )
+				);
+
 				echo '<div class="plugin_box">';
 				echo '<img class="plugin-banner" src="' . $image . '">';
 				echo '<div class="title-action-wrapper">';
@@ -99,11 +113,9 @@ class Upsell extends Abstract_Module {
 				}
 				echo '</div>';
 				echo '<div class="plugin-box-footer">';
-				echo '<div class="button-wrap">';
-				echo self::get_button_html( $plugin );
+				echo '<a class="button thickbox open-plugin-details-modal" href="' . esc_url( $url ) . '">' . esc_html( $strings['install'] ) . '</a>';
 				echo '</div>';
 				echo '<div class="version-wrapper"><span class="version">v' . esc_html( $current_plugin->version ) . '</span></div>';
-				echo '</div>';
 				echo '</div>';
 			}
 
@@ -121,7 +133,10 @@ class Upsell extends Abstract_Module {
 
 				$url = add_query_arg(
 					array(
-						'theme' => $theme->slug,
+						'theme'     => $theme->slug,
+						'TB_iframe' => true,
+						'width'     => 600,
+						'height'    => 500,
 					),
 					network_admin_url( 'theme-install.php' )
 				);
@@ -138,10 +153,9 @@ class Upsell extends Abstract_Module {
 				echo '</div>';
 				echo '<div class="plugin-box-footer">';
 				echo '<div class="button-wrap">';
-				echo '<a class="button" href="' . esc_url( $url ) . '">' . esc_html__( 'Install' ) . '</a>';
+				echo '<a class="button" href="' . esc_url( $url ) . '">' . esc_html( $strings['install'] ) . '</a>';
 				echo '</div>';
 				echo '<div class="version-wrapper"><span class="version">v' . esc_html( $theme->version ) . '</span></div>';
-				echo '</div>';
 				echo '</div>';
 			}
 
@@ -157,14 +171,21 @@ class Upsell extends Abstract_Module {
 	 * @return array|mixed|object
 	 */
 	private function call_theme_api( $slug ) {
+		$theme = get_transient( 'ti_theme_info_' . $slug );
+
+		if ( false !== $theme ) {
+			return $theme;
+		}
+
 		$products = wp_remote_get(
 			'https://api.wordpress.org/themes/info/1.1/?action=query_themes&request[theme]=' . $slug . '&request[per_page]=1'
 		);
 		$products = json_decode( wp_remote_retrieve_body( $products ) );
 		if ( is_object( $products ) ) {
-			return $products->themes[0];
+			$theme = $products->themes[0];
+			set_transient( 'ti_theme_info_' . $slug, $theme, 6 * HOUR_IN_SECONDS );
 		}
-		return null;
+		return $theme;
 	}
 
 	/**
@@ -211,135 +232,6 @@ class Upsell extends Abstract_Module {
 	}
 
 	/**
-	 * Generate action button html.
-	 *
-	 * @param string $slug     plugin slug.
-	 * @param array  $settings button settings.
-	 *
-	 * @return string
-	 */
-	private static function get_button_html( $slug, $settings = array() ) {
-		$button   = '';
-		$redirect = '';
-		if ( ! empty( $settings ) && array_key_exists( 'redirect', $settings ) ) {
-			$redirect = $settings['redirect'];
-		}
-		$state = self::check_plugin_state( $slug );
-		if ( empty( $slug ) ) {
-			return '';
-		}
-
-		$additional = '';
-
-		if ( 'deactivate' === $state ) {
-			$additional = ' action_button active';
-		}
-
-		$button .= '<div class=" plugin-card-' . esc_attr( $slug ) . esc_attr( $additional ) . '" style="padding: 8px 0 5px;">';
-
-		$plugin_link_suffix = self::get_plugin_path( $slug );
-
-		$nonce = add_query_arg(
-			array(
-				'action'        => 'activate',
-				'plugin'        => rawurlencode( $plugin_link_suffix ),
-				'plugin_status' => 'all',
-				'paged'         => '1',
-				'_wpnonce'      => wp_create_nonce( 'activate-plugin_' . $plugin_link_suffix ),
-			),
-			network_admin_url( 'plugins.php' )
-		);
-		switch ( $state ) {
-			case 'install':
-				$button .= '<a data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="install-now ti-install-plugin button  " href="' . esc_url( $nonce ) . '" data-name="' . esc_attr( $slug ) . '" aria-label="Install ' . esc_attr( $slug ) . '">' . __( 'Install and activate' ) . '</a>';
-				break;
-
-			case 'activate':
-				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="activate-now button button-primary" href="' . esc_url( $nonce ) . '" aria-label="Activate ' . esc_attr( $slug ) . '">' . esc_html__( 'Activate' ) . '</a>';
-				break;
-
-			case 'deactivate':
-				$nonce = add_query_arg(
-					array(
-						'action'        => 'deactivate',
-						'plugin'        => rawurlencode( $plugin_link_suffix ),
-						'plugin_status' => 'all',
-						'paged'         => '1',
-						'_wpnonce'      => wp_create_nonce( 'deactivate-plugin_' . $plugin_link_suffix ),
-					),
-					network_admin_url( 'plugins.php' )
-				);
-
-				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" data-slug="' . esc_attr( $slug ) . '" class="deactivate-now button" href="' . esc_url( $nonce ) . '" data-name="' . esc_attr( $slug ) . '" aria-label="Deactivate ' . esc_attr( $slug ) . '">' . esc_html__( 'Deactivate' ) . '</a>';
-				break;
-
-			case 'enable_cpt':
-				$url     = admin_url( 'admin.php?page=jetpack#/settings' );
-				$button .= '<a  data-redirect="' . esc_url( $redirect ) . '" class="button" href="' . esc_url( $url ) . '">' . esc_html__( 'Activate' ) . ' ' . esc_html__( 'Jetpack Portfolio' ) . '</a>';
-				break;
-		}// End switch().
-		$button .= '</div>';
-
-		return $button;
-	}
-
-	/**
-	 * Check plugin state.
-	 *
-	 * @param string $slug - plugin slug.
-	 *
-	 * @return bool
-	 */
-	private static function check_plugin_state( $slug ) {
-
-		$plugin_link_suffix = self::get_plugin_path( $slug );
-
-		if ( file_exists( ABSPATH . 'wp-content/plugins/' . $plugin_link_suffix ) ) {
-			$needs = is_plugin_active( $plugin_link_suffix ) ? 'deactivate' : 'activate';
-			if ( 'deactivate' === $needs && ! post_type_exists( 'portfolio' ) && 'jetpack' === $slug ) {
-				return 'enable_cpt';
-			}
-
-			return $needs;
-		} else {
-			return 'install';
-		}
-	}
-
-
-	/**
-	 * Get plugin path based on plugin slug.
-	 *
-	 * @param string $slug - plugin slug.
-	 *
-	 * @return string
-	 */
-	private static function get_plugin_path( $slug ) {
-
-		switch ( $slug ) {
-			case 'mailin':
-				return $slug . '/sendinblue.php';
-				break;
-			case 'wpforms-lite':
-				return $slug . '/wpforms.php';
-				break;
-			case 'intergeo-maps':
-			case 'visualizer':
-			case 'translatepress-multilingual':
-				return $slug . '/index.php';
-				break;
-			case 'beaver-builder-lite-version':
-				return $slug . '/fl-builder.php';
-				break;
-			case 'adblock-notify-by-bweb':
-				return $slug . '/adblock-notify.php';
-				break;
-			default:
-				return $slug . '/' . $slug . '.php';
-		}
-	}
-
-	/**
 	 * Load css and scripts for the plugin upsell page.
 	 */
 	public function enqueue() {
@@ -355,18 +247,6 @@ class Upsell extends Abstract_Module {
 
 		wp_enqueue_style( 'ti-plugin-style', $this->product->get_base_url( 'vendor/codeinwp/themeisle-sdk/assets/css/upsell.css' ), array(), Loader::get_version() );
 
-		wp_register_script( 'ti-plugin-install', $this->product->get_base_url( 'vendor/codeinwp/themeisle-sdk/assets/js/plugin-install.js' ), array( 'jquery' ), Loader::get_version(), true );
-
-		wp_localize_script(
-			'ti-plugin-install',
-			'tiPluginInstall',
-			array(
-				'activating' => esc_html__( 'Activating ' ),
-			)
-		);
-
 		wp_enqueue_script( 'plugin-install' );
-		wp_enqueue_script( 'updates' );
-		wp_enqueue_script( 'ti-plugin-install' );
 	}
 }
