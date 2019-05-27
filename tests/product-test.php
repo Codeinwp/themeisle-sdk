6bdbe1e5ab37bdec60d080cd8acb95f88742f58d
selul <marius.cristea@vertistudio.com>
2019-05-27 15:02:05 +0300

    fix: change store url with the new domain

--- tests/product-test.php
+++ tests/product-test.php
@@ -23,7 +23,7 @@ class Product_Test extends WP_UnitTestCase {
 		$this->assertEquals( $product->get_store_name(), 'ThemeIsle' );
 		$this->assertEquals( $product->get_version(), '1.1.1' );
 		$this->assertGreaterThanOrEqual( $product->get_install_time(), time() );
-		$this->assertEquals( $product->get_store_url(), 'https://themeisle.com' );
+		$this->assertEquals( $product->get_store_url(), 'https://store.themeisle.com' );
 		$this->assertFalse( $product->requires_license() );
 		$this->assertFalse( $product->is_wordpress_available() );
 	}
@@ -42,7 +42,7 @@ class Product_Test extends WP_UnitTestCase {
 		$this->assertEquals( $product->get_store_name(), 'ThemeIsle' );
 		$this->assertEquals( $product->get_version(), '2.0.18' );
 		$this->assertGreaterThanOrEqual( $product->get_install_time(), time() );
-		$this->assertEquals( $product->get_store_url(), 'https://themeisle.com' );
+		$this->assertEquals( $product->get_store_url(), 'https://store.themeisle.com' );
 		$this->assertTrue( $product->requires_license() );
 		$this->assertTrue( $product->is_wordpress_available() );
 	}
