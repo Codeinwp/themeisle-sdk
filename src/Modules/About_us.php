d2e370d4da29b190181158fe25e0d333e2261285
cristian-ungureanu <cristian@themeisle.com>
2023-08-18 13:02:57 +0300

    fix: phpcs

--- src/Modules/About_us.php
+++ src/Modules/About_us.php
@@ -286,8 +286,8 @@ class About_Us extends Abstract_Module {
 			$pages,
 			function ( $page_data, $page_key ) use ( $product_pages ) {
 				return in_array( $page_key, $product_pages, true ) &&
-				       isset( $page_data['plugin']['status'] ) &&
-				       $page_data['plugin']['status'] === 'not-installed';
+					   isset( $page_data['plugin']['status'] ) &&
+					   $page_data['plugin']['status'] === 'not-installed';
 			},
 			ARRAY_FILTER_USE_BOTH
 		);
