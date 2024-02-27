483899e42c1b002e96986a614ec0e0cdb80c39a6
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2024-02-27 19:07:46 +0200

    feat: script loader module (#213)
    * feat: script loader module
    
    * chore: handler name & js build
    
    * chore: phplint
    
    * test(script-loader): hooks loading
    
    * test(script-loader): hooks functionality
    
    * chore: minor

---
+++ assets/js/src/survey_deps.js
@@ -0,0 +1,10 @@
+import formbricks from "@formbricks/js";
+
+/**
+ * Load the formbricks library and expose it to the global scope.
+ * Emit a custom event to let other scripts know that formbricks is loaded.
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    window.tsdk_formbricks = formbricks;
+    window.dispatchEvent(new Event("themeisle:survey:loaded"));
+});
