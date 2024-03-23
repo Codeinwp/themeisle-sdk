929b44217ea3a2ebe21beaef3b9f4c5c7de8fd87
Bogdan Preda <bogdan.preda@themeisle.com>
2024-03-23 20:18:42 +0200

    chore: added test for the new filter

--- tests/promotion-test.php
+++ tests/promotion-test.php
@@ -73,4 +73,75 @@ class Promotion_Test extends WP_UnitTestCase {
 		$option = get_option( $option_key );
 		$this->assertEquals( 'test', $option );
 	}
+
+	/**
+	 * Test the promotion dissallow filter and the promotion loading without it.
+	 *
+	 * @return void
+	 */
+	public function testPromotionDisallowFilter() {
+		wp_set_current_user( 1 );
+		$file    = dirname( __FILE__ ) . '/sample_products/sample_plugin/plugin_file.php';
+		$product = new \ThemeisleSDK\Product( $file );
+
+		$promotions = new \ThemeisleSDK\Modules\Promotions();
+
+		$this->assertTrue( $promotions->can_load( $product ) );
+		$promotions->load( $product );
+
+		set_current_screen( 'edit-post' );
+		$current_screen = get_current_screen();
+		$current_screen->is_block_editor( true );
+
+		$promotions->load_available();
+		$promotions->enqueue();
+
+		global $wp_scripts; // phpcs:ignore
+		$data                     = $wp_scripts->get_data( 'ti-sdk-promo', 'data' );
+		$prev_data                = $data;
+		$data                     = str_replace( 'var themeisleSDKPromotions = ', '', $data );
+		$data                     = substr( $data, 0, -1 );
+		$themeisle_sdk_promotions = json_decode( $data, true );
+
+		$this->assertEquals( 'Sample plugin.', $themeisle_sdk_promotions['product'] );
+		$this->assertTrue( ! empty( $themeisle_sdk_promotions['showPromotion'] ) );
+
+		add_filter(
+			'sample_plugin_dissallowed_promotions',
+			function () {
+				return [
+					'om-editor',
+					'om-image-block',
+					'om-attachment',
+					'om-media',
+					'om-elementor',
+					'blocks-css',
+					'blocks-animation',
+					'blocks-conditions',
+					'rop-posts',
+					'ppom',
+					'sparks-wishlist',
+					'sparks-announcement',
+					'sparks-product-review',
+				];
+			}
+		);
+
+		$promotions = new \ThemeisleSDK\Modules\Promotions();
+
+		$this->assertTrue( $promotions->can_load( $product ) );
+		$promotions->load( $product );
+		$promotions->load_available();
+		$promotions->enqueue();
+
+		global $wp_scripts; // phpcs:ignore
+		$data = $wp_scripts->get_data( 'ti-sdk-promo', 'data' );
+		// we remove the previous enqueued data to only use the new one.
+		$data                     = str_replace( $prev_data, '', $data );
+		$data                     = str_replace( 'var themeisleSDKPromotions = ', '', $data );
+		$data                     = substr( $data, 0, -1 );
+		$themeisle_sdk_promotions = json_decode( $data, true );
+
+		$this->assertTrue( empty( $themeisle_sdk_promotions['showPromotion'] ) ); // This should be empty as we filter all promotions.
+	}
 }
