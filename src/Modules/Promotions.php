7c06bb8cc6431401ff542832830691507cafb8e0
abaicus <baicusandrei@me.com>
2024-08-05 07:27:59 +0300

    feat: swap Neve FSE notice with Neve [closes Codeinwp/themeisle#1654]

--- src/Modules/Promotions.php
+++ src/Modules/Promotions.php
@@ -71,11 +71,11 @@ class Promotions extends Abstract_Module {
 	private $option_rop = 'themeisle_sdk_promotions_rop_installed';
 
 	/**
-	 * Option key for Neve FSE promos.
+	 * Option key for Neve promo.
 	 *
 	 * @var string
 	 */
-	private $option_neve_fse = 'themeisle_sdk_promotions_neve_fse_installed';
+	private $option_neve = 'themeisle_sdk_promotions_neve_installed';
 
 	/**
 	 * Option key for Redirection for CF7.
@@ -126,10 +126,8 @@ class Promotions extends Abstract_Module {
 		$promotions_to_load[] = 'optimole';
 		$promotions_to_load[] = 'rop';
 		$promotions_to_load[] = 'woo_plugins';
-		$promotions_to_load[] = 'neve-fse';
+		$promotions_to_load[] = 'neve';
 		$promotions_to_load[] = 'redirection-cf7';
-
-		$promotions_to_load = array_unique( $promotions_to_load );
 
 		$promotions_to_load = array_unique( $promotions_to_load );
 
@@ -173,7 +171,7 @@ class Promotions extends Abstract_Module {
 		add_action( 'wp_ajax_tisdk_update_option', array( $this, 'dismiss_promotion' ) );
 		add_filter( 'themeisle_sdk_ran_promos', '__return_true' );
 
-		if ( get_option( $this->option_neve_fse, false ) !== true ) {
+		if ( get_option( $this->option_neve, false ) !== true ) {
 			add_action( 'wp_ajax_themeisle_sdk_dismiss_notice', 'ThemeisleSDK\Modules\Notification::regular_dismiss' );
 		}
 	}
@@ -222,8 +220,8 @@ class Promotions extends Abstract_Module {
 			update_option( 'rop_reference_key', sanitize_key( $_GET['rop_reference_key'] ) );
 		}
 
-		if ( isset( $_GET['neve_fse_reference_key'] ) ) {
-			update_option( 'neve_fse_reference_key', sanitize_key( $_GET['neve_fse_reference_key'] ) );
+		if ( isset( $_GET['neve_reference_key'] ) ) {
+			update_option( 'neve_reference_key', sanitize_key( $_GET['neve_reference_key'] ) );
 		}
 	}
 
@@ -276,7 +274,7 @@ class Promotions extends Abstract_Module {
 		);
 		register_setting(
 			'themeisle_sdk_settings',
-			$this->option_neve_fse,
+			$this->option_neve,
 			array(
 				'type'              => 'boolean',
 				'sanitize_callback' => 'rest_sanitize_boolean',
@@ -348,9 +346,9 @@ class Promotions extends Abstract_Module {
 		$has_redirection_cf7       = defined( 'WPCF7_PRO_REDIRECT_PLUGIN_VERSION' ) || $this->is_plugin_installed( 'wpcf7-redirect' );
 		$had_redirection_cf7_promo = get_option( $this->option_redirection_cf7, false );
 		$is_min_req_v              = version_compare( get_bloginfo( 'version' ), '5.8', '>=' );
-		$is_min_fse_v              = version_compare( get_bloginfo( 'version' ), '6.2', '>=' );
 		$current_theme             = wp_get_theme();
-		$has_neve_fse              = $current_theme->template === 'neve-fse' || $current_theme->parent() === 'neve-fse';
+		$has_neve                  = $current_theme->template === 'neve' || $current_theme->parent() === 'neve';
+		$has_neve_from_promo       = get_option( $this->option_neve, false );
 		$has_enough_attachments    = $this->has_min_media_attachments();
 		$has_enough_old_posts      = $this->has_old_posts();
 
@@ -426,7 +424,7 @@ class Promotions extends Abstract_Module {
 			],
 			'neve'            => [
 				'neve-themes-popular' => [
-					'env'     => ! $has_neve,
+					'env'     => ! $has_neve && ! $has_neve_from_promo,
 					'screen'  => 'themes-install-popular',
 					'delayed' => true,
 				],
@@ -602,8 +600,8 @@ class Promotions extends Abstract_Module {
 			if ( $this->get_upsells_dismiss_time( 'rop-posts' ) === false ) {
 				add_action( 'admin_notices', [ $this, 'render_rop_dash_notice' ] );
 			}
-			if ( $this->get_upsells_dismiss_time( 'neve-fse-themes-popular' ) === false ) {
-				add_action( 'admin_notices', [ $this, 'render_neve_fse_themes_notice' ] );
+			if ( $this->get_upsells_dismiss_time( 'neve-themes-popular' ) === false ) {
+				add_action( 'admin_notices', [ $this, 'render_neve_themes_notice' ] );
 			}
 			if ( $this->get_upsells_dismiss_time( 'redirection-cf7' ) === false ) {
 				add_action( 'admin_notices', [ $this, 'render_redirection_cf7_notice' ] );
@@ -639,8 +637,8 @@ class Promotions extends Abstract_Module {
 			case 'sparks-product-reviews':
 				$this->load_woo_promos();
 				break;
-			case 'neve-fse-themes-popular':
-				// Remove any other notifications if Neve FSE promotion is showing
+			case 'neve-themes-popular':
+				// Remove any other notifications if Neve promotion is showing
 				remove_action( 'admin_notices', array( 'ThemeisleSDK\Modules\Notification', 'show_notification' ) );
 				remove_action(
 					'wp_ajax_themeisle_sdk_dismiss_notice',
@@ -653,7 +651,7 @@ class Promotions extends Abstract_Module {
 				remove_action( 'admin_head', array( 'ThemeisleSDK\Modules\Notification', 'setup_notifications' ) );
 				// Add required actions to display this notification
 				add_action( 'admin_enqueue_scripts', [ $this, 'enqueue' ] );
-				add_action( 'admin_notices', [ $this, 'render_neve_fse_themes_notice' ] );
+				add_action( 'admin_notices', [ $this, 'render_neve_themes_notice' ] );
 				break;
 			case 'wpcf7':
 				add_action( 'admin_enqueue_scripts', [ $this, 'enqueue' ] );
@@ -687,6 +685,12 @@ class Promotions extends Abstract_Module {
 		$asset_file        = require $themeisle_sdk_max_path . '/assets/js/build/promos/index.asset.php';
 		$deps              = array_merge( $asset_file['dependencies'], [ 'updates' ] );
 
+		$themes      = wp_get_themes();
+		$neve_action = 'install';
+		if ( isset( $themes['neve'] ) ) {
+			$neve_action = 'activate';
+		}
+
 		wp_register_script( $handle, $themeisle_sdk_src . 'assets/js/build/promos/index.js', $deps, $asset_file['version'], true );
 		wp_localize_script(
 			$handle,
@@ -707,12 +711,23 @@ class Promotions extends Abstract_Module {
 				'ropActivationUrl'      => $this->get_plugin_activation_link( 'tweet-old-post' ),
 				'optimoleDash'          => esc_url( add_query_arg( [ 'page' => 'optimole' ], admin_url( 'upload.php' ) ) ),
 				'ropDash'               => esc_url( add_query_arg( [ 'page' => 'TweetOldPost' ], admin_url( 'admin.php' ) ) ),
-				'neveFSEMoreUrl'        => tsdk_utmify( 'https://themeisle.com/themes/neve-fse/', 'neve-fse-themes-popular', 'theme-install' ),
 				// translators: %s is the product name.
 				'title'                 => esc_html( sprintf( Loader::$labels['promotions']['recommended'], $this->product->get_name() ) ),
 				'redirectionCF7MoreUrl' => tsdk_utmify( 'https://docs.themeisle.com/collection/2014-redirection-for-contact-form-7', 'redirection-for-contact-form-7', 'plugin-install' ),
 				'rfCF7ActivationUrl'    => $this->get_plugin_activation_link( 'wpcf7-redirect' ),
 				'cf7Dash'               => esc_url( add_query_arg( [ 'page' => 'wpcf7-new' ], admin_url( 'admin.php' ) ) ),
+				'nevePreviewURL'        => esc_url( add_query_arg( [ 'theme' => 'neve' ], admin_url( 'theme-install.php' ) ) ),
+				'neveAction'            => $neve_action,
+				'activateNeveURL'       => esc_url(
+					add_query_arg(
+						[
+							'action'     => 'activate',
+							'stylesheet' => 'neve',
+							'_wpnonce'   => wp_create_nonce( 'switch-theme_neve' ),
+						],
+						admin_url( 'themes.php' ) 
+					)
+				),
 			]
 		);
 		wp_enqueue_script( $handle );
@@ -733,17 +748,17 @@ class Promotions extends Abstract_Module {
 	}
 
 	/**
-	 * Render Neve FSE Themes notice.
+	 * Render Neve Themes notice.
 	 */
-	public function render_neve_fse_themes_notice() {
-		echo '<div id="ti-neve-fse-notice" class="notice notice-info ti-sdk-neve-fse-notice"></div>';
+	public function render_neve_themes_notice() {
+		echo '<div id="ti-neve-notice" class="notice notice-info ti-sdk-om-notice"></div>';
 	}
 
 	/**
 	 * Render Redirection for CF7 notice.
 	 */
 	public function render_redirection_cf7_notice() {
-		echo '<div id="ti-redirection-cf7-notice" class="notice notice-info ti-redirection-cf7-notice"></div>';
+		echo '<div id="ti-redirection-cf7-notice" class="notice notice-info ti-sdk-om-notice"></div>';
 	}
 
 	/**
