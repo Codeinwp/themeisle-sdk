3726c4c9bff26f3446bba8832a48a172e679960e
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-10-23 17:52:33 +0300

    feat: add lang attribute to Formbricks

--- assets/js/src/survey_deps.js
+++ assets/js/src/survey_deps.js
@@ -5,6 +5,18 @@ import formbricks from "@formbricks/js/app";
  * Emit a custom event to let other scripts know that formbricks is loaded.
  */
 document.addEventListener("DOMContentLoaded", () => {
-    window.tsdk_formbricks = formbricks;
+    window.tsdk_formbricks = {
+        init: (args) => {
+            if ( typeof args.attributes === 'object' ) {
+                args.attributes = {
+                    ...window.tsdk_survey_attrs,
+                    ...args.attributes
+                }
+            }
+            
+            formbricks?.init(args)
+        }
+    };
+ 
     window.dispatchEvent(new Event("themeisle:survey:loaded"));
 });
