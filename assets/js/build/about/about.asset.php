82bb1524e57ed449e2dd6a3a55e0893de7c9cecd
harshit <contactharshitarora@gmail.com>
2026-06-28 00:14:23 +0530

    build(about): regenerate about bundle

--- assets/js/build/about/about.asset.php
+++ assets/js/build/about/about.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => '41951f8419d8324fd95b');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element'), 'version' => '2bbb4270e7239a0f1aad');
